a3aa23a753c58c6d9d879a1473d3fe9052b5d98d
Samintha Kaveesh <skaveesh@wiley.com>
2025-01-27 09:52:59 +0530

    adding config reading

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const fs = require('fs');
+const { dialog } = require('electron');
+
+let configJson = null;
 
 function createWindow() {
     const win = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false
+            contextIsolation: false,
+            icon: path.join(__dirname, 'kss_logo.png')
         }
     });
 
@@ -17,12 +21,22 @@ app.whenReady().then(createWindow);
 
 ipcMain.on('load-config', (event) => {
     const configPath = path.join(__dirname, '.config');
-    fs.readFile(configPath, 'utf8', (err, data) => {
+    fs.readFile(configPath, 'utf8', (err, config) => {
         if (err) {
             console.error('Error reading the config file:', err);
+            dialog.showErrorBox('File Read Error', 'Error reading the config file. Please check the file and try again.');
+            app.quit();
             return;
         }
-        event.sender.send('config-data', data);
+        try {
+            configJson = JSON.parse(config);
+            event.sender.send('config-data', configJson);
+        } catch (parseErr) {
+            console.error('Invalid JSON format:', parseErr);
+            dialog.showErrorBox('JSON Parse Error', 'Invalid JSON format in the config file. Please correct the file and try again.');
+            app.quit();
+            return;
+        }
     });
 });
 
