5f694542e1c6fbcdcd7e319df6e4b637ece3eca1
Nipun Dilshan <nidilshan@wiley.com>
2025-01-27 13:34:05 +0530

    remove preload

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
-const { createKafkaClient, produceMessage, consumeMessages, stopConsuming } = require('./backend/kafka');
 const isDev = process.env.NODE_ENV !== 'production';
 
 let mainWindow;
@@ -10,7 +9,6 @@ function createWindow() {
         width: isDev ? 800 : 400,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
             contextIsolation: false,
         },
@@ -22,27 +20,3 @@ function createWindow() {
 }
 
 app.on('ready', createWindow);
-
-ipcMain.on('produce-payload', async (event, payload) => {
-    try {
-        const kafka = await createKafkaClient();
-        await produceMessage(kafka, 'alm-kafka-demo-topic', payload);
-    } catch (error) {
-        event.sender.send('produce-error', error.message);
-    }
-});
-
-ipcMain.on('start-consuming', async (event) => {
-    try {
-        const kafka = await createKafkaClient();
-        await consumeMessages(kafka, 'alm-kafka-demo-topic', 'test-group', (message) => {
-            mainWindow.webContents.send('consumed-message', message);
-        });
-    } catch (error) {
-        event.sender.send('consume-error', error.message);
-    }
-});
-
-ipcMain.on('stop-consuming', async (event) => {
-    await stopConsuming();
-});
